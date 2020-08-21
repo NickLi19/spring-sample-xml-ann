01c8487bfed676b9537c9fbfbb62c3f5f34780cc
Jianmin <nicklee19950330@gmail.com>
2020-08-20 21:15:16 -0500

    spring application using xml and annotation first version

---
+++ src/com/demo/service/CustomerServiceImpl.java
@@ -0,0 +1,28 @@
+package com.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.demo.model.Customer;
+import com.demo.repository.CustomerRepository;
+
+@Service("customerService")
+public class CustomerServiceImpl implements CustomerService {
+	
+	@Autowired
+	private CustomerRepository customerRepository;
+
+	public void setCustomerRepository(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+
+	@Override
+	public List<Customer> findAll() {
+
+		return customerRepository.findAll();
+
+	}
+
+}
